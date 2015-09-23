9af84bba113fc613e95cb62748a43145cacaee82
neo1218 <neo1218@yeah.net>
2015-09-24 00:16:07 +0800

    add 插件安装

--- install.sh
+++ install.sh
@@ -7,26 +7,59 @@
 # ===================
 
 # 开始安装
-echo "starting install n-vim!"
+echo "----starting install n-vim!----"
 cd ~
-
 echo "\n"
 
+
 # .vim
-echo "making .vim"
+echo "----making .vim----"
 cp -r ~/n-vim/ ~/.vim
 
+
 # .vimrc
-echo "making .vimrc"
-cp -r ~/n-vim/vimrc ~/.vimrc
+echo "----making .vimrc----"
+cp -r ~/n-vim/ ~/.vimrc
+
+
+# 安装插件
+echo "----插件安装----"
+cd ~/.vim/bundle/
+
+echo "安装vim-powerline"
+git clone https://github.com/Lokaltog/vim-powerline
+echo "install vim-powerline done !\n"
+
+echo "安装ctrlp"
+git clone https://github.com/kien/ctrlp.vim
+echo "install ctrlp done !\n"
+
+echo "jedi-vim"
+git clone https://github.com/davidhalter/jedi-vim
+echo "install jedi-vim done !\n"
+
+echo "vim-minimap"
+git clone http://github.com/severin-lemaignan/vim-minimap
+echo "install vim-minimap done !\n"
+
+echo "neocomplete.vim"
+git clone https://github.com/Shougo/neocomplete.vim.git
+echo "install neocomplete.vim done! \n"
+
+echo "nerdtree"
+https://github.com/scrooloose/nerdtree
+echo "install nerdtree done! \n"
+
+echo "----install done!----"
+
 
 echo " =============================================================================="
-echo " This is my vim file"
-echo " ref -> https://github.com/mbrochh/vim-as-a-python-ide"
-echo "@neo1218 2015y 9m 17d"
-echo " version: 1.0"
+echo " |                                 n-vim                                      |"
+echo " |          ref -> https://github.com/mbrochh/vim-as-a-python-ide             |"
+echo " |                         @neo1218 2015y 9m 17d                              |"
+echo " |                              version: 1.0                                  |"
 echo " =============================================================================="
-echo " ||  ||  ||====   ||     ||     |====|   O    neo1218"
-echo " ||==||  ||===    ||     ||     |    |  |||           vim on mac for python !"
-echo " ||  ||  ||====   ||__|  ||__|  |====|  \ /     vim"
+echo " ||  ||  ||====   ||     ||     |====|   O    neo1218                         |"
+echo " ||==||  ||===    ||     ||     |    |  |||           vim on mac for python ! |"
+echo " ||  ||  ||====   ||__|  ||__|  |====|  \ /     vim                           |"
 echo " =============================================================================="
