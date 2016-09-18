f9d29c03aa3b926cca63174755d34f0ef04a7395
neo1218 <neo1218@yeah.net>
2016-09-19 04:04:01 +0800

    fix install sh file bug

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 echo "\033[32m(ง •_•)ง =>\033[0m update neovim"
+if [ ! -d "~/.config" ]; then
+    mkdir ~/.config
+    mkdir ~/.config/nvim
+fi
 cp ~/muxivim/init.vim ~/.config/nvim
 cp -rf ~/muxivim/nvim/autoload ~/.config/nvim
 cp -rf ~/muxivim/nvim/colors ~/.config/nvim
 
 echo "\033[32m(ง •_•)ง =>\033[0m Install Extensions"
 nvim +PlugInstall +q!
+
+echo "\033[32m(ง •_•)ง =>\033[0m Config MuxiVim Done!"
